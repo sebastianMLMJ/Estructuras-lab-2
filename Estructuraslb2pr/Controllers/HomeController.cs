4064a1720239d71ca9335965866519467c30df32
sebastianMLMJ <46785324+sebastianMLMJ@users.noreply.github.com>
2019-03-08 17:32:28 -0600

    implementando lista y lectura archivo

---
+++ Estructuraslb2pr/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Estructuraslb2pr.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Collections;
+using System.ComponentModel;
+
+namespace Estructuraslb2pr.Controllers
+{
+   
+    public class HomeController : Controller
+    {
+        public static Lista<Nodo> NuevaLista = new Lista<Nodo>();
+        public static LinkedList<Nodo> Lista = new LinkedList<Nodo>();
+
+        // GET: Home
+        [HttpGet]
+        public ActionResult Index()
+        {
+           
+            return View(NuevaLista);
+        }
+        [HttpGet]
+        public ActionResult Formulario()
+        {
+
+            return View();
+        }
+        /*[HttpPost]
+        public ActionResult Formulario(int Id, string Nombre, string Descripcion, string Casap, double Precio, int Existencia)
+        {
+
+            NuevaLista.Insertar(Id,Nombre,Descripcion,Casap, Precio, Existencia );
+            
+            
+            return View();
+        }*/
+    }
+}
---
+++ Estructuraslb2pr/Models/Lista.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+
+namespace Estructuraslb2pr.Models
+{
+    public class Lista<T>
+    {
+        Nodo primero = new Nodo();
+        Nodo ultimo = new Nodo();
+
+        public Lista()
+        {
+            primero = null;
+            ultimo = null;
+        }
+
+
+        public void Insertar()
+        {
+            Nodo nuevo = new Nodo();
+            string[] lineas = File.ReadLines;
+            foreach (var linea in lineas)
+            {
+                var id = linea.Split(',');
+                string nid = Convert.ToString(id[1]);
+                nid = nid.TrimEnd(',');
+                int Iid = int.Parse(nid);
+                var nombre=id[2].Split(',');
+                string snombre = Convert.ToString(nombre[1]);
+                snombre = snombre.TrimEnd(',');                
+                if (Convert.ToString(nombre[2].Substring(0)) == "\"")
+                {
+                    var separa1 = Convert.ToString(nombre[2]).Split('"');
+                    var descripcion = separa1[2].Split('"');
+                }                
+                var separa2 = descripcion[2].Split('"');
+                var casa = separa2[2].Split('"');
+                var precio = casa[2].Split(',');
+                var existencia = precio[2];
+                nuevo.Id = Iid;
+                nuevo.Nombre = snombre;
+                nuevo.Descripcion = descripcion;
+                nuevo.Casap = casa;
+                nuevo.Precio = precio;
+                nuevo.Existencia = existencia;
+            }
+
+            
+            
+            if (primero == null)
+            {
+                primero = nuevo;
+                ultimo = nuevo;
+                nuevo.siguiente = null;
+            }
+            else
+            {
+                ultimo.siguiente = nuevo;
+                ultimo = nuevo;
+                nuevo.siguiente = null;
+            }
+        }
+
+    }
+}
